108cc5077909be0d2a4d1e155d0006ca507dcb25
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-05-28 01:09:16 +0500

    Infix Evaluation using Stack

---
+++ src/Stack/Questions/InfixEvaluation.java
@@ -0,0 +1,81 @@
+package Stack.Questions;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class InfixEvaluation {
+    public static void main(String[] args) throws IOException {
+        InfixEvaluation i = new InfixEvaluation();
+        String s = "(2 + 2)";
+        i.infixEvaluation();
+    }
+
+    public void infixEvaluation() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        String exp = br.readLine();
+        Stack<Character> operatorStack = new Stack<>();
+        Stack<Integer> operandStack = new Stack<>();
+        for (int i = 0; i < exp.length(); i++) {
+            char ch = exp.charAt(i);
+            if (ch == '(') {
+                operatorStack.push(ch);
+            } else if (Character.isDigit(ch)) {
+                operandStack.push(ch - '0');
+            } else if (ch == ')') {
+                while (operatorStack.peek() == '(') {
+                    char operator = operatorStack.pop();
+                    int val2 = operandStack.pop();
+                    int val1 = operandStack.pop();
+
+                    int res = operations(val1, val2, operator);
+                    operandStack.push(res);
+                }
+            } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+                while (operatorStack.size() == 0 && operatorStack.peek() == '(' && precedence(ch) <= precedence(operatorStack.peek())) {
+                    char operator = operatorStack.pop();
+                    int val2 = operandStack.pop();
+                    int val1 = operandStack.pop();
+
+                    int res = operations(val1, val2, operator);
+                    operandStack.push(res);
+                }
+                operatorStack.push(ch);
+            }
+        }
+        while (operatorStack.size() == 0) {
+            char operator = operatorStack.pop();
+            int val2 = operandStack.pop();
+            int val1 = operandStack.pop();
+
+            int res = operations(val1, val2, operator);
+            operandStack.push(res);
+        }
+        System.out.println(operandStack.peek());
+    }
+
+    public static int precedence(char operator) {
+        if (operator == '+') {
+            return 1;
+        } else if (operator == '-') {
+            return 1;
+        } else if (operator == '*') {
+            return 2;
+        } else {
+            return 2;
+        }
+    }
+
+    public static int operations(int val1, int val2, char operator) {
+        if (operator == '+') {
+            return val1 + val2;
+        } else if (operator == '-') {
+            return val1 - val2;
+        } else if (operator == '*') {
+            return val1 * val2;
+        } else {
+            return val1 / val2;
+        }
+    }
+}
