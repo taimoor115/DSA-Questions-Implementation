8c22e5c5741c63fadf2aa52f492c9a0a1dad17b9
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-06-01 02:01:20 +0500

    TreeNode Helper class

---
+++ src/Trees/Questions/TreeNode.java
@@ -0,0 +1,14 @@
+package Trees.Questions;
+
+public class TreeNode {
+    int data;
+    TreeNode right;
+    TreeNode left;
+
+//    Constructor
+    TreeNode(int data) {
+        this.data = data;
+        this.right = null;
+        this.left = null;
+    }
+}
