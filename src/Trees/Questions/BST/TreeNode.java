ac5f9427c866fad0fc33ac8ae240d6916bd006f6
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-06-05 23:31:03 +0500

    Helper Class

---
+++ src/Trees/Questions/BST/TreeNode.java
@@ -0,0 +1,11 @@
+package Trees.Questions.BST;
+
+public class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int data;
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+}
