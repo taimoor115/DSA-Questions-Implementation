17b64e6341cb6a89c3d81f975e7e662a0dd995f1
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-06-09 11:27:47 +0500

    12. Integer to Roman

---
+++ src/Maths/Questions/IntegerToRoman.java
@@ -0,0 +1,18 @@
+package Maths.Questions;
+//https://leetcode.com/problems/integer-to-roman/
+public class IntegerToRoman {
+    public String intToRoman(int num) {
+        String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] thousand = {"", "M", "MM", "MMM"};
+
+        return thousand[num / 1000] + hundreds[(num % 1000) / 100] + tens[(num % 100) / 10] + ones[num % 10];
+    }
+
+    public static void main(String[] args) {
+        IntegerToRoman i = new IntegerToRoman();
+        System.out.println(i.intToRoman(2111));
+    }
+}
+
