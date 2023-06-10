36fd2c637525d290b9db7b8b2df1a8bb6fcfbc36
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-06-10 22:26:54 +0500

    412. Fizz Buzz

---
+++ src/Maths/Questions/FizzBuzz.java
@@ -0,0 +1,27 @@
+package Maths.Questions;
+
+import java.util.*;
+//https://leetcode.com/problems/fizz-buzz/
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                list.add("Fizz");
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add(String.valueOf(i));
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        FizzBuzz f = new FizzBuzz();
+        f.fizzBuzz(4);
+
+    }
+}
