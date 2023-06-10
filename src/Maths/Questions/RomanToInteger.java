f37a4209b3d832e1ee02d26ed31ced67e17e4dc0
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-06-10 22:25:13 +0500

    13. Roman to Integer

---
+++ src/Maths/Questions/RomanToInteger.java
@@ -0,0 +1,39 @@
+package Maths.Questions;
+
+public class RomanToInteger {
+
+    public int romanToInt(String s) {
+        int number = 0;
+        int answer = 0;
+        int prev = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == 'I') {
+                number = 1;
+            } else if (s.charAt(i) == 'V') {
+                number = 5;
+            } else if (s.charAt(i) == 'X') {
+                number = 10;
+            } else if (s.charAt(i) == 'L') {
+                number = 50;
+            } else if (s.charAt(i) == 'C') {
+                number = 100;
+            } else if (s.charAt(i) == 'D') {
+                number = 500;
+            } else if (s.charAt(i) == 'M') {
+                number = 1000;
+            }
+            if (number < prev) {
+                answer -= number;
+            } else {
+                answer += number;
+            }
+            prev = number;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        RomanToInteger r = new RomanToInteger();
+        System.out.println(r.romanToInt("IV"));
+    }
+}
