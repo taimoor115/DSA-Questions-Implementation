f91932ee131f2c8abef55586e42c6351a6e9c228
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-06-13 11:37:06 +0500

    191. Number of 1 Bits

---
+++ src/Maths/Questions/NumberOf1sBit.java
@@ -0,0 +1,21 @@
+package Maths.Questions;
+//https://leetcode.com/problems/number-of-1-bits/description/
+public class NumberOf1sBit {
+    public int numberOf1bit(int n) {
+        int a = 1;
+        int counter = 0;
+
+        for (int i = 0; i < 32; i++) {
+            if ((n & a) != 0) {
+                counter++;
+            }
+            a <<= 1;
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        NumberOf1sBit n = new NumberOf1sBit();
+        System.out.println(n.numberOf1bit(00000000000000000000000010000000));
+    }
+}
