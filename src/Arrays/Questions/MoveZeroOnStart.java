de7e1c766a5bec287d6ed5343467c9c75074ad3d
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-07-01 02:17:06 +0500

    Move Zero on Left while maintaining the order

--- src/Arrays/Questions/MoveZeroOnStart.java
+++ src/Arrays/Questions/MoveZeroOnStart.java
@@ -4,33 +4,45 @@ import java.util.Arrays;
 
 public class MoveZeroOnStart {
 
-//    public int[] moveLeft(int[] arr) {
-//        int a =
-//    }
-    public int[] moveLeft(int[] arr) {
-        int i = 0;
+    public int[] moveStart(int[] arr) {
+        int i = arr.length - 1;
         int j = arr.length - 1;
 
-        while (i <= j) {
-            if (arr[i] == 0) {
-                i++;
-            } else {
-                swap(arr, i, j);
+        while (i >= 0) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
                 j--;
             }
+            i--;
         }
         return arr;
     }
-
-    private void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+//    public int[] moveLeft(int[] arr) {
+//        int i = 0;
+//        int j = arr.length - 1;
+//
+//        while (i <= j) {
+//            if (arr[i] == 0) {
+//                i++;
+//            } else {
+//                swap(arr, i, j);
+//                j--;
+//            }
+//        }
+//        return arr;
+//    }
+//
+//    private void swap(int[] arr, int i, int j) {
+//        int temp = arr[i];
+//        arr[i] = arr[j];
+//        arr[j] = temp;
+//    }
 
     public static void main(String[] args) {
         MoveZeroOnStart m = new MoveZeroOnStart();
-        int[] arr = {0,2,3,4,5,0,0,2,0};
-        System.out.println(Arrays.toString(m.moveLeft(arr)));
+        int[] arr = {0, 2, 3, 4, 5, 0, 0, 2, 0};
+        System.out.println(Arrays.toString(m.moveStart(arr)));
     }
 }
