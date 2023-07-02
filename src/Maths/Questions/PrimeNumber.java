3593fa02141ceac2cc5eeb3d5e501bf6913d1f92
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-07-02 22:50:49 +0500

    Prime Number

---
+++ src/Maths/Questions/PrimeNumber.java
@@ -0,0 +1,31 @@
+package Maths.Questions;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public boolean isPrime(int number) {
+        if (number == 0) {
+            return false;
+        }
+        if (number < 0) {
+            number = number * -1;
+        }
+        if (number == 1) {
+            return false;
+        }
+
+        int i = number - 1;
+        while (i > 1) {
+            if (number % i == 0) {
+                return false;
+            }
+            i--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        PrimeNumber p = new PrimeNumber();
+        System.out.println(p.isPrime(-11));
+    }
+}
