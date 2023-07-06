6c309edbcae42cdf7c02a493f216851eb8ec41ef
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-07-06 06:24:40 +0500

    LeetCode 1672

---
+++ src/Searching/Questions/Richest_Customer_Wealth.java
@@ -0,0 +1,29 @@
+package Searching.Questions;
+//https://leetcode.com/problems/richest-customer-wealth/description/
+//1672
+public class Richest_Customer_Wealth {
+    public int maximumWealth(int[][] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int temp = 0;
+
+            for (int j = 0; j < arr[i].length; j++) {
+                temp = temp + arr[i][j];
+            }
+            if (temp > sum) {
+                sum = temp;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Richest_Customer_Wealth r = new Richest_Customer_Wealth();
+        int[][] arr = {
+                {1, 2, 2},
+                {1, 2, 3},
+                {1, 2, 4}
+        };
+        System.out.println(r.maximumWealth(arr));
+    }
+}
