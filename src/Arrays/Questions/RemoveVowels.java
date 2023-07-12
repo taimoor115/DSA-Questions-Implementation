725de4ad8ba3d78102caed97ba3ac2f8e9ebd3e2
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-07-12 22:25:54 +0500

    Remove Vowels From String

---
+++ src/Arrays/Questions/RemoveVowels.java
@@ -0,0 +1,25 @@
+package Arrays.Questions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveVowels {
+
+    public List<String> removeVowels(String string) {
+        ArrayList<String> s = new ArrayList<>();
+        char[] arr = string.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!(arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u')) {
+                s.add(String.valueOf(arr[i]));
+            }
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        RemoveVowels r = new RemoveVowels();
+        String string = "Hey It's me Taimoor Hussain!";
+        System.out.println(r.removeVowels(string));
+    }
+}
