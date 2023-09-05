8681a6181585384353d52a5394fe5fbc35a72815
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-09-06 01:11:55 +0500

    Order Agnostic Binary Search

---
+++ src/Sorting/OrderIgnosticBS.java
@@ -0,0 +1,42 @@
+package Sorting;
+
+public class OrderIgnosticBS {
+    static int orderAgnosticBS(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        // find whether the array is sorted in ascending or descending
+        boolean isAsc = arr[start] < arr[end];
+
+        while (start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        OrderIgnosticBS o = new OrderIgnosticBS();
+        int[] arr = {7, 6, 4, 2, 1};
+        System.out.println(orderAgnosticBS(arr, 6));
+    }
+}
