4843dea2e6f2a7a10b41010b6328c52ba813ce83
Taimoor Hussain <taimoorhussain985@gmail.com>
2023-09-14 08:37:27 +0500

    LeetCode-9

--- src/Maths/Questions/Palindrome.java
+++ src/Maths/Questions/Palindrome.java
@@ -4,26 +4,38 @@ package Maths.Questions;
 //09
 public class Palindrome {
 
+//    public boolean isPalindrome(int x) {
+//        String y = String.valueOf(x);
+//        char[] arr = y.toCharArray();
+//
+//        int s = 0;
+//        int e = arr.length - 1;
+//
+//        while (s < e) {
+//            if (arr[s] != arr[e]) {
+//                return false;
+//            } else {
+//                s++;
+//                e--;
+//            }
+//        }
+//        return true;
+//    }
+
     public boolean isPalindrome(int x) {
-        String y = String.valueOf(x);
-        char[] arr = y.toCharArray();
-
-        int s = 0;
-        int e = arr.length - 1;
-
-        while (s < e) {
-            if (arr[s] != arr[e]) {
-                return false;
-            } else {
-                s++;
-                e--;
-            }
+        int reversed = 0;
+        int number = x;
+        while (x > 0) {
+            int digit = x % 10;
+            reversed = (reversed * 10) + digit;
+            x = x / 10;
         }
-        return true;
+
+        return number == reversed;
     }
 
     public static void main(String[] args) {
-        int x = 120;
+        int x = -121;
         Palindrome p = new Palindrome();
         System.out.println(p.isPalindrome(x));
     }
